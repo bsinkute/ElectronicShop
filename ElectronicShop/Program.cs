3d77551102cb813f1e88e59ea1242d21eea07994
ramunasdapkus@gmail.com <ramunasdapkus@gmail.com>
2024-04-02 21:18:03 +0300

    Added UserWindowSelection Class. Chamged in Main lines15-17

--- ElectronicShop/Program.cs
+++ ElectronicShop/Program.cs
@@ -1,5 +1,4 @@
 ﻿using ElectronicShop.Models;
-
 namespace ElectronicShop
 {
     internal class Program
@@ -13,33 +12,9 @@ namespace ElectronicShop
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
             UserWindow userWindow = new UserWindow();
-            userWindow.LoadUserWindow(out int  userWindowSelection);
-            switch (userWindowSelection)
-            {
-                case 1:
-                    Console.Clear();
-                    Console.WriteLine("User Settings--- Method");
-                    break;
-                case 2:
-                    Console.Clear();
-                    Console.WriteLine("Cart--- Method");
-                    break;
-                case 3:
-                    Console.Clear();
-                    Console.WriteLine("Go To Shop--- Method");
-                    break;
-                case 4:
-                    Console.Clear();
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("You Loged Out");
-                    Console.ResetColor();
-                    Console.WriteLine("Shop Load--- Method");
-                    break;
-                default:
-                    Console.WriteLine("ERROR");
-                    break;
-
-            }
+            UserWindowSelection selection = new UserWindowSelection();
+            userWindow.LoadUserWindow(out int userWindowSelection);
+            selection.Selecter(userWindowSelection);
         }
     }
 }
