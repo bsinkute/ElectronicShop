a8fbf4bc04526368e8c6103701440a03d57ec8e0
ramunasdapkus@gmail.com <ramunasdapkus@gmail.com>
2024-04-02 20:39:43 +0300

    User Window Created. In Main created Switch from User window selection.

--- ElectronicShop/Program.cs
+++ ElectronicShop/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ElectronicShop
+﻿using ElectronicShop.Models;
+
+namespace ElectronicShop
 {
     internal class Program
     {
@@ -10,6 +12,34 @@
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
+            UserWindow userWindow = new UserWindow();
+            userWindow.LoadUserWindow(out int  userWindowSelection);
+            switch (userWindowSelection)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.WriteLine("User Settings--- Method");
+                    break;
+                case 2:
+                    Console.Clear();
+                    Console.WriteLine("Cart--- Method");
+                    break;
+                case 3:
+                    Console.Clear();
+                    Console.WriteLine("Go To Shop--- Method");
+                    break;
+                case 4:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("You Loged Out");
+                    Console.ResetColor();
+                    Console.WriteLine("Shop Load--- Method");
+                    break;
+                default:
+                    Console.WriteLine("ERROR");
+                    break;
+
+            }
         }
     }
 }
