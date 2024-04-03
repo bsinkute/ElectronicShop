de595d247b1559f57858399686781b4980d39c7f
Bronislava <sinkute.bronislava@gmail.com>
2024-04-03 15:49:14 +0300

    Add admin window and admin window selections

---
+++ ElectronicShop/Models/AdminWindow.cs
@@ -0,0 +1,26 @@
+﻿namespace ElectronicShop.Models
+{
+    public class AdminWindow
+    {
+        public void LoadAdminSettingsWindow(out int adminSettingsSelection)
+        {
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("User Settings:");
+                Console.WriteLine("1. Add New Item \r\n2. Edit Item \r\n3. User Review \r\n4. Delete User \r\n5. Log Out");
+                bool isCorectSelection = int.TryParse(Console.ReadLine(), out adminSettingsSelection);
+                if (!isCorectSelection || adminSettingsSelection < 1 || adminSettingsSelection > 5)
+                {
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Next time enter number from 1 to 4");
+                    Console.ResetColor();
+                    Console.ReadLine();
+                    continue;
+                }
+                else break;
+            }
+        }
+    }
+}
