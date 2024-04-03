9d39a2f7ce6e4457829fccae5ea3f4fa93c07263
Augustas Tupciauskas <augustast@icloud.com>
2024-04-03 18:39:02 +0300

    added properties for future tasks

---
+++ ElectronicShop/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicShop
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal Wallet { get; set; }
+        public bool IsAdmin { get; set; }
+        private string _filePath = @"C:\Users\User\OneDrive\Desktop\.NET\ElectronicStore_Project\Models\users.txt";
+        private string _emailAddress;
+    }
+}
