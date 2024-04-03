2fbf53edf5f6bb7c60a5b18432491b91d40868b9
Bronislava <sinkute.bronislava@gmail.com>
2024-04-03 17:18:37 +0300

    Fix coments

--- ElectronicShop/Models/AdminWindow.cs
+++ ElectronicShop/Models/AdminWindow.cs
@@ -7,14 +7,14 @@
             while (true)
             {
                 Console.Clear();
-                Console.WriteLine("User Settings:");
+                Console.WriteLine("Admin Window:");
                 Console.WriteLine("1. Add New Item \r\n2. Edit Item \r\n3. User Review \r\n4. Delete User \r\n5. Log Out");
                 bool isCorectSelection = int.TryParse(Console.ReadLine(), out adminSettingsSelection);
                 if (!isCorectSelection || adminSettingsSelection < 1 || adminSettingsSelection > 5)
                 {
                     Console.Clear();
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Next time enter number from 1 to 4");
+                    Console.WriteLine("Next time enter number from 1 to 5");
                     Console.ResetColor();
                     Console.ReadLine();
                     continue;
