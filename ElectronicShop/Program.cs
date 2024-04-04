059e7ea5c197f05004a999ac2f808a83d8412c9e
Bronislava <sinkute.bronislava@gmail.com>
2024-04-04 12:31:38 +0300

    Revert changes made to program.cs

--- ElectronicShop/Program.cs
+++ ElectronicShop/Program.cs
@@ -11,26 +11,14 @@ namespace ElectronicShop
             Console.OutputEncoding = System.Text.Encoding.Unicode;
             Console.WriteLine("Hello, World!");
             Console.WriteLine("Hello, World!");
-            AdminWindow adminWindow = new AdminWindow();
-            AdminWindowSelection adminWindowSelection = new AdminWindowSelection();
-
-            int adminSettingsSelection;
-            adminWindow.LoadAdminSettingsWindow(out adminSettingsSelection);
-
-            adminWindowSelection.Selector(adminSettingsSelection);
-
-            Console.ReadLine();
-
-
-            /*UserWindow userWindow = new UserWindow();
+            UserWindow userWindow = new UserWindow();
             UserWindowSelection selection = new UserWindowSelection();
             userWindow.LoadUserWindow(out int userWindowSelection);
             selection.Selecter(userWindowSelection);
             UserSettingsWindow userSettingsWindow = new UserSettingsWindow();
             UserSettingsSelecter userSettingsSelecter = new UserSettingsSelecter();
             userSettingsWindow.LoadUserSettingsWindow(out int userSettingsSelection);
-            userSettingsSelecter.UserSettingsWindowSelecter(userSettingsSelection);*/
+            userSettingsSelecter.UserSettingsWindowSelecter(userSettingsSelection);
         }
-        
     }
 }
